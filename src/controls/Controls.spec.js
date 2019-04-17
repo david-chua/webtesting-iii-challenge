8b8bf56d944a61b1de0b937f06c5c6a3f5036e71
david-chua <dp_chua@hotmail.com>
2019-04-16 22:13:17 -0400

    default fireevent works

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,18 @@
 // Test away!
+import React from 'react';
+import Controls from './Controls';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+afterEach(cleanup);
+
+describe('The Controls component', () => {
+  it('default should you cannot lock the gate since it is still open', () =>{
+    const { getByText, queryByText } = render(<Controls />);
+
+    const lockButton = getByText(/lock gate/i);
+    fireEvent.click(lockButton);
+
+    getByText(/lock gate/i)
+    getByText(/close gate/i);
+
+  })
+})
