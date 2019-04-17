7aa8b3aaaa9713e84aaef08f37351d1bd0928dc0
david-chua <dp_chua@hotmail.com>
2019-04-16 21:32:34 -0400

    default in display is working

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+import React from 'react';
+import Display from './Display';
+import { render, fireEvent } from 'react-testing-library';
+import renderer from 'react-test-renderer';
+
+describe('The Display component', () => {
+
+  it('default display is unlocked and open', () => {
+    const { getByText } = render(<Display />);
+
+    getByText(/Unlocked/i);
+    getByText(/Open/i);
+  });
+
+})
